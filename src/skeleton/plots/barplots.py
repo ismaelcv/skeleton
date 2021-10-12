86a181444a4237425dd6c2d9d398d09f003a627f
ismaelcv <ismaelcv@gmail.com>
2021-10-12 17:33:28 +0200

    add last changes to bar graph

--- src/skeleton/plots/barplots.py
+++ src/skeleton/plots/barplots.py
@@ -19,11 +19,15 @@ def plot_percentage_per_subcategory(
     secondary: name of the category to aggregate in the dataframe
     """
 
-    per_cat_perc = ratio_per_category(df, primary, secondary)
+    per_cat_perc = ratio_per_category(df, primary, secondary)  # .sort_values(by=secondary, key=sort_order)
 
-    pivot_per_cat_perc = pd.pivot_table(
-        per_cat_perc, values=f"perc_{secondary}", index=primary, columns=secondary, aggfunc=np.sum
-    ).round(1)
+    pivot_per_cat_perc = (
+        pd.pivot_table(
+            per_cat_perc, values=f"perc_{secondary}", index=primary, columns=secondary, aggfunc=np.sum
+        )
+        .round(1)
+        .fillna(0)
+    )
 
     category_names = pivot_per_cat_perc.columns
     labels = list(pivot_per_cat_perc.index)
@@ -39,13 +43,33 @@ def plot_percentage_per_subcategory(
     for i, (colname, color) in enumerate(zip(category_names, category_colors)):
         widths = data[:, i]
         starts = data_cum[:, i] - widths
-        print(colname)
-
-        rects = ax.barh(labels, widths, left=starts, height=0.5, label=colname, color=color)
+        ax.barh(labels, widths, left=starts, height=0.8, label=colname, color=color)
+        xcenters = starts + widths / 2
 
         r, g, b, _ = color
         text_color = "white" if r * g * b < 0.5 else "darkgrey"
-        ax.bar_label(rects, label_type="center", color=text_color, size=17, weight="bold")
-    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1), loc="lower left", fontsize=15)
+        for y, (x, c) in enumerate(zip(xcenters, widths)):
+            if c > 3:
+                ax.text(
+                    x, y, str(c) + "%", ha="center", va="center", color=text_color, size=18, weight="bold"
+                )
+
+    ax.tick_params(axis="both", labelsize=20, color="black")
+
+    # Remove contour
+    ax.spines["left"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1), loc="lower left", fontsize="15")
 
     return fig, ax
+
+
+def _sorter(df: pd.DataFrame, primary: str, secondary: str) -> pd.Series:
+    """Sort function"""
+    cats = list(df.groupby(primary)[secondary].mean().sort_values().index)
+
+    correspondence = {cats: order for order, cats in enumerate(cats)}
+    return df[secondary].map(correspondence)
