6df0521acf0a5586b431078ca075ad38696006e1
ismaelcv <ismaelcv@gmail.com>
2021-12-31 03:54:07 +0100

    first pipeline setup

--- src/skeleton/plots/scatter.py
+++ src/skeleton/plots/scatter.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Optional, Union
 
 import pandas as pd
@@ -13,23 +15,23 @@ class graph:
     Generic class to genererate a plt graph
     """
 
-    def __init__(self, df: pd.DataFrame, x: str, y: str, style: str = "base") -> None:
+    def __init__(self, df: pd.DataFrame, x: str, y: str) -> None:
 
         plt.rcParams.update(plt.rcParamsDefault)
 
         self.df = df
         self.x = x
         self.y = y
-        self.styleParams = STYLES[style]["styleParams"]
-        self.rcParams = STYLES[style]["rcParams"]
-        self.colors = STYLES[style]["colors"]
+        self.styleParams = STYLES["base"]["styleParams"]
+        self.rcParams = STYLES["base"]["rcParams"]
+        self.colors = STYLES["base"]["colors"]
         self._set_figsize()
         self.z = ""  # type: str
         self.main_categories = []  # type: list
         self.notes = []  # type: list
-        self.style = STYLES[style]
+        self.style = STYLES["base"]
 
-    def color_by(self, column_name: str) -> None:
+    def color_by(self, column_name: str) -> graph:
         """
         Set the color for the plot.
 
@@ -55,6 +57,8 @@ class graph:
             )
         else:
             print(f"Possible categories to .focus_on() {available_values}")
+
+        return self
 
     def focus_on(self, category: Union[str, list]) -> None:
         """
@@ -99,6 +103,18 @@ class graph:
 
         self.main_categories = list(pd.Series(self.main_categories).unique())
 
+    def set_style(self, style: str) -> graph:
+
+        if style in STYLES:
+            self.style = STYLES[style]
+            self.styleParams = STYLES[style]["styleParams"]
+            self.rcParams = STYLES[style]["rcParams"]
+            self.colors = STYLES[style]["colors"]
+        else:
+            raise ValueError(f"{style} is not a valid style")
+
+        return self
+
     def set_title(self, title: str) -> None:
         self.styleParams["title_style"]["text"] = title
 
@@ -119,7 +135,7 @@ class graph:
             if key in self.styleParams:
                 self.styleParams[key] = value
 
-    def add_plot_style(self) -> None:
+    def _apply_style(self) -> None:
 
         if "text" in self.styleParams["title_style"]:
             plt.annotate(**self.styleParams["title_style"])
@@ -223,59 +239,6 @@ class graph:
                 """
             )
 
-
-class base_scatter(graph):
-    def show(self) -> None:
-
-        if self.z != "":
-            categories = self.df[self.z].unique().tolist()
-
-            if len(categories) == 0:
-                raise ValueError("No categories found: Length of categories is 0")
-        else:
-            categories = []
-
-        plt.rcParams.update(self.rcParams)
-
-        if len(categories) <= 1:
-
-            color = self.colors["1cat"]
-            plt.scatter(self.df[self.x], self.df[self.y], color=color, **self.style["scatter_style"])
-
-        else:
-            colors = self.colors["ncats"]
-
-            for category in categories:
-
-                if category not in self.main_categories:
-
-                    x_axis = self.df[self.x][self.df[self.z] == category]
-                    y_axis = self.df[self.y][self.df[self.z] == category]
-
-                    plt.scatter(
-                        x_axis, y_axis, color=self.colors["grayed"], **self.style["scattershadow_style"]
-                    )
-
-            for i, category in enumerate(self.main_categories):
-
-                if len(self.main_categories) == 1:
-                    color = self.colors["1cat"]
-                else:
-                    color = colors[i % len(colors)]
-
-                x_axis = self.df[self.x][self.df[self.z] == category]
-                y_axis = self.df[self.y][self.df[self.z] == category]
-
-                plt.scatter(x_axis, y_axis, color=color, **self.style["scatter_style"])
-
-        for note, kwargs in self.notes:
-
-            plt.annotate(**note, **kwargs)
-
-        self.add_plot_style()
-
-
-class base_lineplot(graph):
     def show(self) -> None:
 
         plt.rcParams.update(self.rcParams)
@@ -286,8 +249,6 @@ class base_lineplot(graph):
                 raise ValueError("No categories found: Length of categories is 0")
         else:
             categories = []
-
-        plt.rcParams.update(self.rcParams)
 
         if len(categories) <= 1:
             color = self.colors["1cat"]
@@ -317,8 +278,114 @@ class base_lineplot(graph):
 
                 plt.plot(x_axis, y_axis, color=color, **self.style["line_style"])
 
-        for note, kwargs in self.notes:
+        plt.show()
 
-            plt.annotate(**note, **kwargs)
 
-        self.add_plot_style()
+# class base_scatter(graph):
+#     def show(self) -> None:
+
+#         if self.z != "":
+#             categories = self.df[self.z].unique().tolist()
+
+#             if len(categories) == 0:
+#                 raise ValueError("No categories found: Length of categories is 0")
+#         else:
+#             categories = []
+
+#         plt.rcParams.update(self.rcParams)
+
+#         if len(categories) <= 1:
+
+#             color = self.colors["1cat"]
+#             plt.scatter(self.df[self.x], self.df[self.y], color=color, **self.style["scatter_style"])
+
+#         else:
+#             colors = self.colors["ncats"]
+
+#             for category in categories:
+
+#                 if category not in self.main_categories:
+
+#                     x_axis = self.df[self.x][self.df[self.z] == category]
+#                     y_axis = self.df[self.y][self.df[self.z] == category]
+
+#                     plt.scatter(
+#                         x_axis, y_axis, color=self.colors["grayed"], **self.style["scattershadow_style"]
+#                     )
+
+#             for i, category in enumerate(self.main_categories):
+
+#                 if len(self.main_categories) == 1:
+#                     color = self.colors["1cat"]
+#                 else:
+#                     color = colors[i % len(colors)]
+
+#                 x_axis = self.df[self.x][self.df[self.z] == category]
+#                 y_axis = self.df[self.y][self.df[self.z] == category]
+
+#                 plt.scatter(x_axis, y_axis, color=color, **self.style["scatter_style"])
+
+#         for note, kwargs in self.notes:
+
+#             plt.annotate(**note, **kwargs)
+
+#         self.add_plot_style()
+
+#         plt.show()
+
+
+# class base_lineplot(graph):
+#     def show(self) -> None:
+
+#         plt.rcParams.update(self.rcParams)
+
+#         if self.z != "":
+#             categories = self.df[self.z].unique().tolist()
+#             if len(categories) == 0:
+#                 raise ValueError("No categories found: Length of categories is 0")
+#         else:
+#             categories = []
+
+
+#         if len(categories) <= 1:
+#             color = self.colors["1cat"]
+#             plt.plot(self.df[self.x], self.df[self.y], color=color, **self.style["line_style"])
+
+#         else:
+#             colors = self.colors["ncats"]
+
+#             for category in categories:
+
+#                 if category not in self.main_categories:
+
+#                     x_axis = self.df[self.x][self.df[self.z] == category]
+#                     y_axis = self.df[self.y][self.df[self.z] == category]
+
+#                     plt.plot(x_axis, y_axis, color=self.colors["grayed"], **self.style["lineshadow_style"])
+
+#             for i, category in enumerate(self.main_categories):
+
+#                 if len(self.main_categories) == 1:
+#                     color = self.colors["1cat"]
+#                 else:
+#                     color = colors[i % len(colors)]
+
+#                 x_axis = self.df[self.x][self.df[self.z] == category]
+#                 y_axis = self.df[self.y][self.df[self.z] == category]
+
+#                 plt.plot(x_axis, y_axis, color=color, **self.style["line_style"])
+
+#         for note, kwargs in self.notes:
+
+#             plt.annotate(**note, **kwargs)
+
+#         self._apply_style()
+
+#         plt.show()
+
+
+# class base_scatter(graph):
+
+
+class plot(graph):
+    ...
---
+++ src/skeleton/plots/styles/vox.py
@@ -0,0 +1,65 @@
+from skeleton.plots.colors import plt_tab_colors
+
+style = {
+    "rcParams": {
+        # Background color
+        "axes.facecolor": "#3a4849",
+        "figure.facecolor": "#3a4849",
+        # Labels settings
+        "axes.labelcolor": "white",
+        "axes.labelsize": 20,
+        "axes.labelpad": 25,
+        # XY line axis color
+        "axes.edgecolor": "white",
+        # XY line width
+        "axes.linewidth": 4,
+        # XY line remove top/right
+        "axes.spines.right": False,
+        "axes.spines.top": False,
+        # Change xy axis color
+        "xtick.color": "white",
+        "ytick.color": "white",
+        "xtick.major.size": 10,
+        "xtick.major.width": 3,
+        "ytick.major.size": 10,
+        "ytick.major.width": 3,
+        # Y grid
+        "grid.color": "lightgray",
+        "grid.alpha": 0.4,
+        "axes.grid": True,
+        "axes.grid.axis": "y",
+        # Savefig properties
+        "savefig.facecolor": "black",
+        "savefig.edgecolor": "black",
+    },
+    "scatter_style": {
+        "s": 40,
+    },
+    "line_style": {
+        "linewidth": 1.5,
+    },
+    "scattershadow_style": {
+        "s": 40,
+    },
+    "lineshadow_style": {
+        "linewidth": 1,
+    },
+    "styleParams": {
+        "xticks": {"rotation": 45, "fontsize": 13, "fontweight": "bold"},
+        "yticks": {"fontsize": 15, "fontweight": "bold"},
+        "title_style": {
+            "fontsize": 24,
+            "color": "white",
+            "fontweight": "bold",
+            "xy": (0.00, 1.11),
+            "xycoords": "axes fraction",
+        },
+        "subtitle_style": {
+            "color": "red",
+            "fontsize": 20,
+            "xy": (0.00, 1.05),
+            "xycoords": "axes fraction",
+        },
+    },
+    "colors": {"1cat": "red", "ncats": plt_tab_colors, "grayed": "lightgray"},
+}
